245517c754e6c09061a3a959949d5a2d82328ca3
mf0zz13 <michael.foster31@outlook.com>
2024-09-19 22:09:26 -0400

    Added additonal Tests for Selection Sort

--- SortingAlgorithmsTests/SortingAlgorithmTests.cs
+++ SortingAlgorithmsTests/SortingAlgorithmTests.cs
@@ -3,13 +3,55 @@ using SortingAlgoithms;
 namespace SortingAlgoithmTest
 {
     [TestClass]
-    public class SortingAlgorithmTests
+    public class SelectionSort
     {
         [TestMethod]
         public void SelectionSort_EmptyArray()
         {
             //Arrange
+            int[] array = new int[0];
+            int[] arrayControl = new int[0];
+
+            //Act
+            Program.SelectionSort(array);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void SelectionSort_TestCaseOne()
+        {
+            //Arrange
+            int[] array = new int[1] { 0 };
+            int[] arrayControl = new int[1] { 0 };
+
+            //Act
+            Program.SelectionSort(array);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void SelectionSort_TestCaseTwo()
+        {
+            //Arrange
             int[] array = new int[5] { 5, 4, 3, 2, 1 };
+            int[] arrayControl = new int[5] { 1, 2, 3, 4, 5 };
+
+            //Act
+            Program.SelectionSort(array);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void SelectionSort_TestCaseThree()
+        {
+            //Arrange
+            int[] array = new int[5] { 3, 5, 1, 2, 4 };
             int[] arrayControl = new int[5] { 1, 2, 3, 4, 5 };
 
             //Act
