81ad853e26131a8f2b782b0bcf07bf9df8db74bb
mf0zz13 <michael.foster31@outlook.com>
2024-09-20 11:22:49 -0400

    Added Merge Sort methods and tests

--- Sorting Algorithm/Program.cs
+++ Sorting Algorithm/Program.cs
@@ -24,5 +24,17 @@
             //TODO: implement Insertion Sort
 
         }
+
+        public static void MergeSort_Divide(int[] array, int left, int right)
+        {
+            //TODO: implement Merge Sort, use MergeSort_Divide and MergeSort_Merge
+        }
+
+        public static void MergeSort_Merge(int[] A, int left, int mid, int right)
+        {
+            
+        }
+
+
     }
 }
--- SortingAlgorithmsTests/SortingAlgorithmTests.cs
+++ SortingAlgorithmsTests/SortingAlgorithmTests.cs
@@ -181,5 +181,65 @@ namespace SortingAlgoithmTests
             CollectionAssert.AreEqual(array, arrayControl);
         }
     }
+
+    [TestClass]
+    public class MergeSort
+    {
+        [TestMethod]
+        public void MergeSort_EmptyArray()
+        {
+            //Arrange
+            int[] array = new int[0];
+            int[] arrayControl = new int[0];
+
+            //Act
+            Program.MergeSort_Divide(array, 0, array.Length -1);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void MergeSort_TestCaseOne()
+        {
+            //Arrange
+            int[] array = new int[1] { 0 };
+            int[] arrayControl = new int[1] { 0 };
+
+            //Act
+            Program.MergeSort_Divide(array, 0, array.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void MergeSort_TestCaseTwo()
+        {
+            //Arrange
+            int[] array = new int[5] { 5, 5, 3, 2, 1 };
+            int[] arrayControl = new int[5] { 1, 2, 3, 5, 5 };
+
+            //Act
+            Program.MergeSort_Divide(array, 0, array.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+
+        [TestMethod]
+        public void MergeSort_TestCaseThree()
+        {
+            //Arrange
+            int[] array = new int[5] { 3, 5, 1, 2, 4 };
+            int[] arrayControl = new int[5] { 1, 2, 3, 4, 5 };
+
+            //Act
+            Program.MergeSort_Divide(array, 0, array.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(array, arrayControl);
+        }
+    }
 }
 
